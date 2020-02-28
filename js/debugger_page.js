e191f1a9e80eb977352833a3fbd5543463fcdb83
MauliPatel24 <60676935+MauliPatel24@users.noreply.github.com>
2020-02-28 08:17:32 -0500

    Update debugger_page.js

--- js/debugger_page.js
+++ js/debugger_page.js
@@ -1,5 +1,20 @@
-document.getElementById("client_file").onchange = function() {myFunction()};
+document.getElementById("client_file").onchange = function() {read_client_file()};
 
-function myFunction() {
-console.log('client upload local file');
+function read_client_file() {
+  console.log('client upload local file');
+  var files = document.getElementById("client_file").files;
+  if(files.length>0){
+    console.log("1 file");
+    var file = files[0];
+    var allowed_types = [ 'text/plain' ];
+    if(allowed_types.indexOf(file.type) == -1) {
+		  alert('Error : Incorrect file type');
+		  return;
+	  }
+    
+    var reader = new FileReader();
+    reader.readAsText(file);
+    var text = reader.result;
+    console.log('text');
+  }
 }
