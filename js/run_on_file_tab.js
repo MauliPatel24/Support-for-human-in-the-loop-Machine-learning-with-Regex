f2fbf62be72f42f45d6d7f672ee43deea97a9448
MauliPatel24 <60676935+MauliPatel24@users.noreply.github.com>
2020-03-03 10:34:33 -0500

    Update run_on_file_tab.js
    added selector for page

--- js/run_on_file_tab.js
+++ js/run_on_file_tab.js
@@ -1,5 +1,13 @@
 var run_on_file = {
+    
     list_of_files :[],
     currrent_index : -1,
-    regex_body : function() {return $('#regex_2_3').val();}
+    regex_body : function() {return $('#regex_2_3');},
+    regex_option : function() { return $('#options_2'); },
+    btn_clear_all : function () { return $('#clear_all');},
+    btn_load_all_files : function () { return $('#get_all_files');},
+    lbl_available_files : function () { return $('#available_files');},
+    file_selector : function() {return $('#Select_files');},
+    btn_process_files : function () { return $('#process_selected_files');}
+    
 }
