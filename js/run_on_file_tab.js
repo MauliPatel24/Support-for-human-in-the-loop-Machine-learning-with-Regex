3fbe6a420dd85992d5da5a749861fba11c05c067
MauliPatel24 <60676935+MauliPatel24@users.noreply.github.com>
2020-03-03 21:45:53 -0500

    Update run_on_file_tab.js
    created (declare) validation function for process_files button on click event

--- js/run_on_file_tab.js
+++ js/run_on_file_tab.js
@@ -12,6 +12,10 @@ var run_on_file = {
     
     update_lable_available_files : function () {
         run_on_file.lbl_available_files().text(`${run_on_file.list_of_files.length - run_on_file.currrent_index }`+ "Files are available to process");
+    },
+
+    validation_to_process_files : function () {
+        return true;
     }
 };
 
@@ -23,3 +27,7 @@ run_on_file.btn_load_all_files().click(function (){
     run_on_file.update_lable_available_files(); //call function to update lable for #of files
 });
 
+run_on_file.btn_process_files().click(function (){
+    var isvalid = run_on_file.validation_to_process_files();
+    console.log(isvalid);
+});
