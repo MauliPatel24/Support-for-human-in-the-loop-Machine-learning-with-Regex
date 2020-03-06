288e29f6477233ed8061a420732e8fef4899ff99
MauliPatel24 <60676935+MauliPatel24@users.noreply.github.com>
2020-03-06 13:20:38 -0500

    Update run_on_file_tab.js

--- js/run_on_file_tab.js
+++ js/run_on_file_tab.js
@@ -3,7 +3,7 @@ var run_on_file = {
     list_of_files :[],
     currrent_index : 0,
     //available_files : list_of_files.length - currrent_index,
-    available_files :run_on_file.list_of_files == undefined ? 0 : list_of_files.length - currrent_index,
+    available_files :list_of_files == undefined ? 0 : list_of_files.length - currrent_index,
     regex_body : function() {return $('#regex_2_3');},
     regex_option : function() { return $('#options_2'); },
     btn_clear_all : function () { return $('#clear_all');},
