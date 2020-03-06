fbb435548f982a149eccb1948a039545491be08f
MauliPatel24 <60676935+MauliPatel24@users.noreply.github.com>
2020-03-06 12:29:09 -0500

    Update run_on_file_tab.js
    element enable disable on process files btn gets clicked

--- js/run_on_file_tab.js
+++ js/run_on_file_tab.js
@@ -2,7 +2,7 @@ var run_on_file = {
     
     list_of_files :[],
     currrent_index : 0,
-available_files : run_on_file.list_of_files.length - run_on_file.currrent_index ;
+    available_files : run_on_file.list_of_files.length - run_on_file.currrent_index,
     regex_body : function() {return $('#regex_2_3');},
     regex_option : function() { return $('#options_2'); },
     btn_clear_all : function () { return $('#clear_all');},
@@ -16,20 +16,25 @@ available_files : run_on_file.list_of_files.length - run_on_file.currrent_index 
     },
 
     validation_to_process_files : function () {
-if(run_on_file.regex_body().val() == "" || run_on_file.regex_body().val() == null){
-alert("Please Enter Regex to proceed");
-return false;
-}
+        if(run_on_file.regex_body().val() == "" || run_on_file.regex_body().val() == null){
+            alert("Please Enter Regex to proceed");
+            return false;
+        }
 
-if(run_on_file.list_of_files.length == 0){
-alert("Please Load All Files to Proceed");
-return false;
-}
+        if(run_on_file.list_of_files.length == 0){
+            alert("Please Load All Files to Proceed");
+            return false;
+        }
 
-if(run_on_file.file_selector().val() == null){
-alert("Please Select Files to Proceed");
-return false;
-}
+        if(run_on_file.file_selector().val() == null){
+            alert("Please Select Files to Proceed");
+            return false;
+        }
+        
+        if(run_on_file.available_files <= 0){
+            alert("No Available Files to process");
+            return false;
+        }
 
         return true;
     }
@@ -44,6 +49,24 @@ run_on_file.btn_load_all_files().click(function (){
 });
 
 run_on_file.btn_process_files().click(function (){
+    //as soon as btn clicked then some effecting element get dis able
+    run_on_file.regex_body().prop( "disabled", true );
+    run_on_file.btn_load_all_files().prop( "disabled", true );
+    run_on_file.file_selector().prop( "disabled", true );
+    
+    //doing validation for all possible senario
     var isvalid = run_on_file.validation_to_process_files();
+    
+    //if all validation get passed then [1] create regex, [2] it will run for loop for every file, [3] in loop read each file, 
+    //[4] apply regex and create result list/set, [5] create/append result table
+    if(isvalid == true ){
+        //do stuff when pass validation
+        console.log("validation get passed");
+    }
     console.log(isvalid);
+    
+    //validation pass or fail it will enable all element which was disabled ealier.
+    run_on_file.regex_body().prop( "disabled", false );
+    run_on_file.btn_load_all_files().prop( "disabled", false );
+    run_on_file.file_selector().prop( "disabled", false );
 });
